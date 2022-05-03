f3b7a9f41b22886e593534f2517cc90e01ef6f92
Kander Akinci <k.akinci@bkgl.bgl.schule>
2022-05-03 14:16:39 +0200

    Projektdateien hinzufügen.

---
+++ MyTeam.IsBetter=true (Kryptograph)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTeam.IsBetter_true__Kryptograph_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
