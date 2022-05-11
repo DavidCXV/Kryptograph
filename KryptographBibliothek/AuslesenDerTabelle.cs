971cde6834c6d31ff305588242ba80175a61fbd0
David Waderna <david.w15@gso.schule.koeln>
2022-05-11 12:25:16 +0200

    Anfang Liste

--- KryptographBibliothek/AuslesenDerTabelle.cs
+++ KryptographBibliothek/AuslesenDerTabelle.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace KryptographBibliothek
 {
@@ -7,8 +9,55 @@ namespace KryptographBibliothek
 
         public static void AuslesenTab()
         {
-            Console.WriteLine("test");
+            List<double> Wahrscheinlichkeiten = new List<double>();
+            Wahrscheinlichkeiten.Add(0.558);
+            Wahrscheinlichkeiten.Add(0.054);
+            Wahrscheinlichkeiten.Add(0.196);
+            Wahrscheinlichkeiten.Add(0.316);
+            Wahrscheinlichkeiten.Add(0.498);
+            Wahrscheinlichkeiten.Add(1.693);
+            Wahrscheinlichkeiten.Add(0.149);
+            Wahrscheinlichkeiten.Add(0.302);
+            Wahrscheinlichkeiten.Add(0.498);
+            Wahrscheinlichkeiten.Add(0.802);
+            Wahrscheinlichkeiten.Add(0.24);
+            Wahrscheinlichkeiten.Add(0.132);
+            Wahrscheinlichkeiten.Add(0.360);
+            Wahrscheinlichkeiten.Add(0.255);
+            Wahrscheinlichkeiten.Add(0.1053);
+            Wahrscheinlichkeiten.Add(0.224);
+            Wahrscheinlichkeiten.Add(0.030);
+            Wahrscheinlichkeiten.Add(0.067);
+            Wahrscheinlichkeiten.Add(0.002);
+            Wahrscheinlichkeiten.Add(0.689);
+            Wahrscheinlichkeiten.Add(0.037);
+            Wahrscheinlichkeiten.Add(0.642);
+            Wahrscheinlichkeiten.Add(0.579);
+            Wahrscheinlichkeiten.Add(0.383);
+            Wahrscheinlichkeiten.Add(0.065);
+            Wahrscheinlichkeiten.Add(0.084);
+            Wahrscheinlichkeiten.Add(0.178);
+            Wahrscheinlichkeiten.Add(0.005);
+            Wahrscheinlichkeiten.Add(0.005);
+            Wahrscheinlichkeiten.Add(0.121);
+
+          
+
+            Console.WriteLine(Wahrscheinlichkeiten[0]);
+
+            //public static void rückgabe();
+            {
+
+            }
         }
+
+        
+        
+        
+
+
+    
+
 
     }
 }
