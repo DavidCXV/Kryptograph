131a27f9aa18721b2c30a1b1e97d688834450893
Dennis Jakob <dennis.j6@gso.schule.koeln>
2022-05-13 11:27:24 +0200

    Anfang des Codes

--- KryptographBibliothek/ZeichenAusgeben.cs
+++ KryptographBibliothek/ZeichenAusgeben.cs
@@ -1,8 +1,37 @@
 ﻿using System;
 
+
 namespace KryptographBibliothek
 {
     public class ZeichenAusgeben
     {
+      public static void ZeichenAu() 
+        {
+
+            Console.WriteLine("Ihr Ausgegebner Text lautet : \r");
+            Console.WriteLine("____________________________\n");
+
+            
+            Console.WriteLine("Wählen sie Zwischen den Folgenden Optionen : ");
+            
+
+            Console.WriteLine("Wählen sie eine möglichkeit aus");
+            Console.WriteLine("\t - test1 ");
+            Console.WriteLine("\t - test2 ");
+            Console.WriteLine("\t - test3");
+            Console.WriteLine("\t - test4 ");
+            Console.WriteLine("Wähle eine Option");
+
+            string auswahl;
+
+
+
+
+
+
+
+
+
+        }
     }
 }
