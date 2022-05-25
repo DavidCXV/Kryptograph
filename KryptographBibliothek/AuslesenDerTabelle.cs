19c88962efe0a5e8447f4910d1bb851007713e9e
David Waderna <david.w15@gso.schule.koeln>
2022-05-25 11:05:48 +0200

    Weiter gekommen als vorher

--- KryptographBibliothek/AuslesenDerTabelle.cs
+++ KryptographBibliothek/AuslesenDerTabelle.cs
@@ -9,47 +9,33 @@ namespace KryptographBibliothek
 
         public static void AuslesenTab()
         {
-            double[,] Wahrscheinlichkeiten = new double[,] { {0.558, 0.054, 0.196, 0.316, 0.498, 1.693, 0.149, 0.302, 0.498, 0.802, 0.24, 0.132, 0.360, 0.255, 0.1053, 0.224,
-            0.030, 0.067, 0.002, 0.689, 0.037, 0.642, 0.579, 0.383, 0.065, 0.084, 0.178, 0.005, 0.005, 0.121   }, };
+            double[] Wahrscheinlichkeiten = {0.558, 0.054, 0.196, 0.316, 0.498, 1.693, 0.149, 0.302, 0.498, 0.802, 0.24, 0.132, 0.360, 0.255, 0.1053, 0.224,
+            0.030, 0.067, 0.002, 0.689, 0.037, 0.642, 0.579, 0.383, 0.065, 0.084, 0.178, 0.005, 0.005, 0.121   };
 
-            string[,] Wahrsch_Buchst = new string[,] { { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" } };
-
-
-
+            string[] Wahrsch_Buchst = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"  };
 
             var alles = ("", 0);
 
-            bool überg = false;
+            bool überg = double.TryParse(Wahrscheinlichkeiten, in Wahrsch_Buchst);
 
             switch (überg)
             {
                 case false:
-             //       alles = Übergabe(Wahrscheinlichkeiten);
+                    alles = Übergabe(Wahrscheinlichkeiten);
                     break;
 
 
                 case true:
-             //     alles = Übergabe(Wahrsch_Buchst);
+                    alles = Übergabe(Wahrsch_Buchst);
                     break;
 
-
-
-
-
-
-
-
-
             }
-
-            static void Übergabe(double[,]Wahrsch, string[,]Busta)
+            static void Übergabe(double[] Wahrsch, string[] Buchsta)
             {
+            
+                
 
             }
-
-
-
-
         }
             
     }
