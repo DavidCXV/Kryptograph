7ae63ca7ad887f216881db2190e62fd44f8f0c5e
Nicolai Pascal Mockenhaupt <nicolaipa.m@gso.schule.koeln>
2022-05-25 12:39:35 +0200

    Tabellen Sortiert und erster Sortierungs Entwurf

--- KryptographBibliothek/ZeichenErsetzen.cs
+++ KryptographBibliothek/ZeichenErsetzen.cs
@@ -1,16 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace KryptographBibliothek
 {
     public class ZeichenErsetzen
     {
-        public static string Zeichenersetzen(string Chiffre,Dictionary<string,int> Buchstaben,Dictionary<string,double> Wahrscheinlichkeiten)
+        public static string Zeichenersetzen(string chiffre,Dictionary<string,int> Buchstaben,Dictionary<string,double> Wahrscheinlichkeiten)
         {
-            string text = "Hello";
-           
-            
-            return text;
+            //beise Dictionarys sortieren
+            ///
+
+
+
+            var sortedDict_chiffre = from entry in Buchstaben orderby entry.Value ascending select entry;
+            var sortedDict_deutsch = from entry in Wahrscheinlichkeiten orderby entry.Value ascending select entry;
+
+
+
+            //tabelle_zeichen_chiffre = tabelle_zeichen_chiffre.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+
+
+
+
+
+            for (int i = 0; i < Buchstaben.Count; i++)
+            {
+
+
+
+                chiffre.Replace(sortedDict_chiffre.ElementAt(sortedDict_chiffre.Count() - i - 1).Key, sortedDict_deutsch.ElementAt(sortedDict_deutsch.Count() - i - 1).Key);
+
+
+
+                Console.WriteLine(chiffre);
+            }
+
+
+
+            // klartext auf Deutsch = Khf ez
+            // Klartext auf Englisch = Ber by
+
+
+
+            // Sortiert Value
+        
+    
+
+
+
+return chiffre;
         }
     }
 }
