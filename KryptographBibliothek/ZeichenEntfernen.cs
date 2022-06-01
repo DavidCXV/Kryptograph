091f39f9dc9d5564b9ded6d798c0bec2a790a9ca
Moritz Mersmann <moritz.m41@gso.schule.koeln>
2022-06-01 10:50:58 +0200

    farbe geändert

--- KryptographBibliothek/ZeichenEntfernen.cs
+++ KryptographBibliothek/ZeichenEntfernen.cs
@@ -7,6 +7,9 @@ namespace KryptographBibliothek
     {
         public static void Entfernen()
         {
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
             Console.WriteLine("Das ist ihr verschlüsselter Text:");
 
             string chiffre = "Kly ulbl KbhsZluzl Jvuayvssly pza pu Mburapvuzbtmhun buk Clyhyilpabun lpul rshyl Clyilzzlybun gbt KbhsZovjr 4, hsslykpunz mpls tpy kpl nlypunl Hrrb-Shbmglpa däoyluk klz Alzaz ulnhapc hbm, kpl cvy hsslt Jvyl Nhtly zaöylu küymal.Lilumhssz upjoa glpanltäß pza kpl Nyößl kly pualyulu Mlzawshaal, dlsjol upjoa 1 AI, zvuklyu uby 825 NI nyvß pza, cvu klulu dplklybt uby 667 NI nlubaga dlyklu röuulu, kh kly Ylza cvt Zfzalt ilslna dpyk. Pu Glpalu, pu klulu Zwplsl hbjo zjovuths ipz gb 100 NI nyvß zpuk, dlyklu oply zjoulss Wshagwyvisltl luazalolu.Kpl Rvuzvsl bualyzaüaga lydlpalyal Zwlpjolytönspjorlpalu pu Mvyt cvu BZI - Shbmdlyrlu, hsslykpunz röuulu oply uby WZ4 - Zwplsl hbznlshnlya dlyklu. Kly pualyul Zwlpjoly rhuu müy WZ5-Zwplsl tpa ZZK-Lydlpalybunzrhyalu(khur T.2.- Zjoupaazalssl) clynyößlya dlyklu, hsslykpunz dpyk kplzlz Mlahbyl ilp Ylslhzl uvjo upjoa gby Clymünbunz zalolu, kluu lz pza lyza müy lpu gbrüumapnlz Bwkhal nlwshua. Lz islpia higbdhyalu, dpl zpjo kpl WZ5 pt Shunglpaalza zjoshnlu dpyk. Khur Hidäyazrvtwhapipspaäa, klu Shbujo - Apalsu buk klu Lersbzpc - Zwplslu khym hily qlaga zjovu nlyul tpa Zvufz Ulea-Nlu - Rvuzvsl nlspliäbnlsa dlyklu";          
@@ -40,6 +43,7 @@ namespace KryptographBibliothek
                 Console.WriteLine("Welches Zeichen möchten sie nun entfernen?\n\n");  //Der Benutzer wird gefragt welches Zeichen er entfernen möchte
                 string Zeichen2 = Convert.ToString(Console.ReadLine());  // Der Nutzer gibt das zu entfernende Zeichen ein und es wird eingelesen
                 string correctString2 = correctString.Replace(Zeichen2, "");
+                Console.WriteLine("Nun können sie mit dem nächsten Schritt fortfahren");
 
                 bool flag2 = true;
 
