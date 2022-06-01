363b35ed77cd216b8dfa899bb18f14e331b9cf54
David S <david.s58@gso.schule.koeln>
2022-06-01 19:31:35 +0200

    Menue für die Chiffre vollendet

--- KryptographBibliothek/Menue.cs
+++ KryptographBibliothek/Menue.cs
@@ -9,120 +9,71 @@ namespace KryptographBibliothek
 
         public static void MainMenue()
         {
-
-            string HauptAusw;
-
-            bool Exit = false;
-            bool Hmenue;
-
-            do
+            string HauptAusw; bool Exit = false;
+            bool Mreturn; do
             {
-                (int, int) cPosBM = Console.GetCursorPosition();
-
-                Console.WriteLine();
-
-                Console.ForegroundColor = ConsoleColor.Cyan;
-
-                //ASCII art Logo wird erzeugt.
+                (int, int) cPosBM = Console.GetCursorPosition(); Console.WriteLine(); Console.ForegroundColor = ConsoleColor.Cyan; //ASCII art Logo wird erzeugt.
                 Console.WriteLine
-                    (FiggleFonts.Slant.Render("BFT - Kryptograph - Substitutions-Chiffre"));
-
-                Console.ResetColor();
-
-
-                Console.ForegroundColor = ConsoleColor.Cyan;
-
-                //Konsolentitel wird geändert.
-                Console.Title = "BFT - Kryptograph - Substitutions-Chiffre";
-
-                Console.ForegroundColor = ConsoleColor.White;
-
-                Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                                  "                              >>> Kryptograph Menue <<<\n" +
-                                  "------------------------------------------------------------------------------------\n\n");
-
-                Console.ForegroundColor = ConsoleColor.Cyan;
-
-                Console.WriteLine("Eingabe: exit\t\t->\tbeendet das Programm");
-
-
-                //Beschreibung der Software.
+                (FiggleFonts.Slant.Render("BFT - Kryptograph - Substitutions-Chiffre")); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan; //Konsolentitel wird geändert.
+                Console.Title = "BFT - Kryptograph - Substitutions-Chiffre"; Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                " >>> Kryptograph Menue <<<\n" +
+                "------------------------------------------------------------------------------------\n\n"); Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine("Eingabe: exit\t\t->\tbeendet das Programm");
+                Console.WriteLine("Eingabe: Mreturn\t\t->\tkehrt zum Auswahlmenue zurueck"); //Beschreibung der Software.
                 Console.WriteLine("\n\nDieses Modul bietet die Möglichkeit"
-                                    + "einen verschlüsselten Text, mit dem Substitutions-Chiffren Prinzip zu entschlüsseln. \n\n");
-
-                Hmenue = false;
-
-                //Eingabeaufforderung 
-
+                + "einen verschlüsselten Text, mit einem Prinzip zu entschlüsseln. \n\n"); Mreturn = false; //Eingabeaufforderung
+                Console.WriteLine("Mit welcher Chiffre wollen sie arbeiten: ");
+                Console.Write("1 - Substitutions-Chiffre\n");
+                Console.Write("2 - Caesar-Chiffre\n");
+                Console.Write("3 - Skytale-Chifre\n");
                 Console.Write("Eingabe:");
-                HauptAusw = Console.ReadLine();
-
-                switch (HauptAusw)
+                HauptAusw = Console.ReadLine(); switch (HauptAusw)
                 {
-
                     case "exit":
                         Exit = true;
                         break;
-
                     case "1":
-
+                        Substitution();
+                        break;
+                    case "2":
+                        Caesar();
+                        break;
+                    case "3":
+                        Skytale();
                         break;
                     default:
-
-
-
-
-
                         (int, int) cPosAM = Console.GetCursorPosition();
-
-
-                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
-
-                        break;
+                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2); break;
                 }
-
-
-            } while (!Exit & !Hmenue);
-
-            if (Exit)
-                Environment.Exit(0);
-
-            Console.ResetColor();
-
+            } while (!Exit); if (Exit)
+                Environment.Exit(0); Console.ResetColor();
         }
-
-
         public static string Pfadabfrage()
         {
-            string fullPath = " ";
-
-
-            do
+            string fullPath = " "; do
             {
                 Console.WriteLine("Warte auf Eingabe eines Dateipfads");
-                string curFile = Console.ReadLine();
-
-                string[] paths = { @"", curFile };
+                string curFile = Console.ReadLine(); string[] paths = { @"", curFile };
                 fullPath = Path.Combine(paths);
-
-
-                Console.WriteLine(File.Exists(fullPath) ? "File exists." : "File does not exist.");
-            } while (!File.Exists(fullPath));
-
-            return fullPath;
-
-
-
-
+                Console.WriteLine(File.Exists(fullPath) ? "Datei existiert." : "Datei existiert nicht.");
+                Console.ReadKey();
+            } while (!File.Exists(fullPath)); return fullPath;
         }
-        
         public static void Substitution()
         {
-
             string pfad = Pfadabfrage();
-
         }
+        public static void Caesar()
+        {
+            Console.WriteLine("Dieses Entschlüsselungsprinzip ist noch in Arbeit");
+        }
+        public static void Skytale()
+        {
+            Console.WriteLine("Dieses Entschlüsselungsprinzip ist noch in Arbeit");
+        }
+               
     }
-}    
-    
+}
+
+
 
--- StartKryptograph/main.cs
+++ StartKryptograph/main.cs
@@ -7,7 +7,7 @@ namespace StartKryptograph
         static void Main(string[] args)
         {
             KryptographBibliothek.Menue.MainMenue();
-
+            
 
             //string curFile = @"C:\Users\david.s58.SCHULE.000\source\repos\MyTeam.IsBetter-true_Kryptograph\Chiffe.txt";
             //Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
