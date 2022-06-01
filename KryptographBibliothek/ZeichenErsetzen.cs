623c91808b4ef804e820171746d4bcbbb3d74447
Nicolai Pascal Mockenhaupt <nicolaipa.m@gso.schule.koeln>
2022-06-01 12:46:45 +0200

    absolute verzweiflung

--- KryptographBibliothek/ZeichenErsetzen.cs
+++ KryptographBibliothek/ZeichenErsetzen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace KryptographBibliothek
 {
@@ -8,7 +9,7 @@ namespace KryptographBibliothek
     {
         public static string Zeichenersetzen(string chiffre,Dictionary<string,int> Buchstaben,Dictionary<string,double> Wahrscheinlichkeiten)
         {
-            //beise Dictionarys sortieren
+            //beide Dictionarys sortieren
             ///
 
 
@@ -20,37 +21,42 @@ namespace KryptographBibliothek
 
             //tabelle_zeichen_chiffre = tabelle_zeichen_chiffre.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 
+            for(int i = 0;i<chiffre.Length;i++)
+            {
+                char zeichen = chiffre[i];
 
+                if(sortedDict_chiffre.ToString().Contains(zeichen))
+                {
+                    int index = sortedDict_chiffre.ToList().IndexOf(zeichen);
 
-
-
-
+                    Console.WriteLine(index);
+                
+                }
+            
+            }
+                
+                
             for (int i = 0; i < Buchstaben.Count; i++)
             {
 
 
 
-                chiffre.Replace(sortedDict_chiffre.ElementAt(sortedDict_chiffre.Count() - i - 1).Key, sortedDict_deutsch.ElementAt(sortedDict_deutsch.Count() - i - 1).Key);
+                chiffre = chiffre.Replace(sortedDict_chiffre.ElementAt(sortedDict_chiffre.Count() - i - 1).Key, sortedDict_deutsch.ElementAt(sortedDict_deutsch.Count() - i - 1).Key);
 
-
-
-                Console.WriteLine(chiffre);
             }
 
 
 
-            // klartext auf Deutsch = Khf ez
-            // Klartext auf Englisch = Ber by
-
-
 
             // Sortiert Value
-        
-    
 
 
 
-return chiffre;
+
+
+            return chiffre;
         }
+
+
     }
 }
