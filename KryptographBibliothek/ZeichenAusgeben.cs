52a372d0a7dd7ac2ba4ae7aa50901856267b74b0
Dennis Jakob <dennis.j6@gso.schule.koeln>
2022-06-01 09:47:33 +0200

    Fertiger Code kommentiert

--- KryptographBibliothek/ZeichenAusgeben.cs
+++ KryptographBibliothek/ZeichenAusgeben.cs
@@ -10,33 +10,36 @@ namespace KryptographBibliothek
       public static void ZeichenAu() 
         {
 
-            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Cyan; // Farbe auf Cyan-blau geändert
 
-            (int, int) cPosBM = Console.GetCursorPosition();
+            (int, int) cPosBM = Console.GetCursorPosition();  // Zeiger wird geladen
 
-            Console.WriteLine();
+            Console.WriteLine(); 
 
            
             Console.WriteLine
-                (FiggleFonts.Slant.Render("Ausgabe der Chiffre"));
+                (FiggleFonts.Slant.Render("Ausgabe der Chiffre")); // Benutze Figgle um den Text zu Rendern
 
            
-            Console.Title = "Ausgabe der Chiffre";
+            Console.Title = "Ausgabe der Chiffre"; // Title auf Ausgabe der Chiffre geändert
 
 
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.White; // Farbe auf weiß geändert
 
 
-
+           
 
             Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                              "                              >>> Ausgabe der Chiffre <<<\n" +
+                              "                              >>> Ausgabe der Chiffre <<<\n" +                                       // Überschrift
                               "------------------------------------------------------------------------------------\n\n");
 
-            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Cyan; // Farbe auf Cyan-blau geändert
 
-            Console.WriteLine("Ihr Ausgegebener Text lautet :  ");
-                Console.ReadKey();
+            Console.WriteLine("Ihr Ausgegebener Text lautet : Test  ");   // Ausgabe des Fertigen Textes
+            Console.ReadKey();                                                                               // Wartet auf eingabe um das Programm zu beenden
+                
+
+
             
 
             
