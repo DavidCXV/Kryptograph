9e147e2fdfaacac8775f7868e9a0099ad9d70cb0
Nicolai Pascal Mockenhaupt <nicolaipa.m@gso.schule.koeln>
2022-06-03 12:42:50 +0200

    fast da

--- KryptographBibliothek/ZeichenErsetzen.cs
+++ KryptographBibliothek/ZeichenErsetzen.cs
@@ -7,31 +7,47 @@ namespace KryptographBibliothek
 {
     public class ZeichenErsetzen
     {
-        public static string Zeichenersetzen(string chiffre,Dictionary<string,int> Buchstaben,Dictionary<string,double> Wahrscheinlichkeiten)
+        public static string Zeichenersetzen(string chiffre,Dictionary<string,double> Buchstaben,Dictionary<string,double> Wahrscheinlichkeiten)
         {
             //beide Dictionarys sortieren
             ///
 
 
 
-            var sortedDict_chiffre = from entry in Buchstaben orderby entry.Value ascending select entry;
-            var sortedDict_deutsch = from entry in Wahrscheinlichkeiten orderby entry.Value ascending select entry;
+            var sortedDict_chiffre = from entry in Buchstaben orderby entry.Value descending select entry;
+            var sortedDict_deutsch = from entry in Wahrscheinlichkeiten orderby entry.Value descending select entry;
 
+            //sortedDict_deutsch.ToDictionary<KeyValuePair<string, double>, string, double>(pair => pair.Key,pair=>pair.Value);
 
 
             //tabelle_zeichen_chiffre = tabelle_zeichen_chiffre.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 
-            for(int i = 0;i<chiffre.Length;i++)
-            {
-                char zeichen = chiffre[i];
-
-                if(sortedDict_chiffre.ToString().Contains(zeichen))
+                for(int i = 0;i<chiffre.Length;i++)
                 {
-                    int index = sortedDict_chiffre.ToList().IndexOf(zeichen);
+                    char symbols = chiffre[i];
 
-                    Console.WriteLine(index);
+                    if(sortedDict_chiffre.ToString().Contains(symbols))
+                    {
+                        int index = 0;
+                        int count = 0;
+                        foreach (KeyValuePair<string, double> element in sortedDict_chiffre) 
+                        {
+                            count++;
+                            if (element.Key == chiffre[i].ToString())
+                                index = count;
+
+                        }
+
+
+                        Console.WriteLine(sortedDict_chiffre);
+                        //chiffre[i] = sortedDict_deutsch.ElementAt(index);
+                        //string text = chiffre[i].ToString().Replace(sortedDict_chiffre.ElementAt(sortedDict_chiffre.Count() - index - 1).Key, sortedDict_deutsch.ElementAt(sortedDict_deutsch.Count() - index - 1).Key);
+
+                    
+
+                        //Console.WriteLine(text);
                 
-                }
+                    }
             
             }
                 
@@ -58,5 +74,7 @@ namespace KryptographBibliothek
         }
 
 
+
+        
     }
 }
