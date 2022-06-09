1e76a6bb13f5a760508f1c04eaf4e7184e6b9b21
Nicolai Pascal Mockenhaupt <nicolaipa.m@gso.schule.koeln>
2022-06-09 11:53:45 +0200

    fertig

--- KryptographBibliothek/ZeichenErsetzen.cs
+++ KryptographBibliothek/ZeichenErsetzen.cs
@@ -2,75 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Text;
 
 namespace KryptographBibliothek
 {
     public class ZeichenErsetzen
     {
-        public static string Zeichenersetzen(string chiffre,Dictionary<string,double> Buchstaben,Dictionary<string,double> Wahrscheinlichkeiten)
+        public static string Zeichenersetzen(string chiffre,Dictionary<string,double> chiffre_tabelle,Dictionary<string,double> sprache_tabelle)
         {
-            //beide Dictionarys sortieren
-            ///
+            chiffre = chiffre.ToUpper();
 
 
+            sprache_tabelle = sprache_tabelle.OrderByDescending(u => u.Value).ToDictionary(z => z.Key, y => y.Value);
+            chiffre_tabelle = chiffre_tabelle.OrderByDescending(u => u.Value).ToDictionary(z => z.Key, y => y.Value);
 
-            var sortedDict_chiffre = from entry in Buchstaben orderby entry.Value descending select entry;
-            var sortedDict_deutsch = from entry in Wahrscheinlichkeiten orderby entry.Value descending select entry;
-
-            //sortedDict_deutsch.ToDictionary<KeyValuePair<string, double>, string, double>(pair => pair.Key,pair=>pair.Value);
-
-
-            //tabelle_zeichen_chiffre = tabelle_zeichen_chiffre.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-
-                for(int i = 0;i<chiffre.Length;i++)
-                {
-                    char symbols = chiffre[i];
-
-                    if(sortedDict_chiffre.ToString().Contains(symbols))
-                    {
-                        int index = 0;
-                        int count = 0;
-                        foreach (KeyValuePair<string, double> element in sortedDict_chiffre) 
-                        {
-                            count++;
-                            if (element.Key == chiffre[i].ToString())
-                                index = count;
-
-                        }
+            StringBuilder ch = new StringBuilder(chiffre);
 
 
-                        Console.WriteLine(sortedDict_chiffre);
-                        //chiffre[i] = sortedDict_deutsch.ElementAt(index);
-                        //string text = chiffre[i].ToString().Replace(sortedDict_chiffre.ElementAt(sortedDict_chiffre.Count() - index - 1).Key, sortedDict_deutsch.ElementAt(sortedDict_deutsch.Count() - index - 1).Key);
+            int index = 0;
 
-                    
-
-                        //Console.WriteLine(text);
-                
-                    }
-            
-            }
-                
-                
-            for (int i = 0; i < Buchstaben.Count; i++)
+            for (int i = 0; i < chiffre.Length; i++)
             {
+                for (int r = 0; r < chiffre_tabelle.Count; r++)
+                { if (chiffre_tabelle.Keys.ElementAt(r) == chiffre[i].ToString())
+                    {
+                        index = r;
 
+                        ch[i] = sprache_tabelle.Keys.ElementAt(index).ToCharArray()[0];
 
-
-                chiffre = chiffre.Replace(sortedDict_chiffre.ElementAt(sortedDict_chiffre.Count() - i - 1).Key, sortedDict_deutsch.ElementAt(sortedDict_deutsch.Count() - i - 1).Key);
-
+                    }
+                        
+               
+                            
+                }
             }
 
 
 
 
-            // Sortiert Value
 
-
-
-
-
-            return chiffre;
+            return ch.ToString();
         }
 
 
